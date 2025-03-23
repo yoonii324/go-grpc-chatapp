43f679d20242cdedcd2d1443803445756608fe33
yoonii324 <jyoonii324@gmail.com>
2025-03-23 02:35:15 -0500

    echoServer methods: OnOpen, OnClose, OnBoot, OnTraffic

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
+
 import (
-	"flag"
-	"fmt"
 	"log"
 
 	gnet "github.com/panjf2000/gnet/v2"
@@ -10,7 +9,33 @@ import (
 type echoServer struct {
 	gnet.BuiltinEventEngine
 
-	eng gnet.BuiltinEventEngine
-	addr string
+	eng       gnet.Engine
+	addr      string
 	multicore bool
+}
+
+// OnOpen is calleed when a new client joins
+func (es *echoServer) OnOpen(c gnet.Conn) (out []byte, action gnet.Action) {
+	log.Printf("client connected. address:%s", c.RemoteAddr().String())
+	return nil, gnet.None
+}
+
+// OnClose is calleed when a client disconnects
+func (es *echoServer) OnClose(c gnet.Conn, err error) (action gnet.Action) {
+	log.Printf("client disconnected. address:%s", c.RemoteAddr().String())
+	return gnet.None
+}
+
+// OnBoot is called when the server starts
+func (es *echoServer) OnBoot(eng gnet.Engine) gnet.Action {
+	es.eng = eng
+	log.Printf("echo server with multi-core=%t is listening on %s\n", es.multicore, es.addr)
+	return gnet.None
+}
+
+// OnTraffic is called when the server receives data from the network
+func (es *echoServer) OnTraffic(c gnet.Conn) gnet.Action {
+	buf, _ := c.Next(-1)
+	c.Write(buf)
+	return gnet.None
 }
